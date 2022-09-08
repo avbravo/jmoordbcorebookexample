0c81a929a3b723fbd0b9b264c0cae800c65fcf4e
avbravo <avbravo@avbravo>
2022-09-08 17:33:14 -0500

    capitulo 4

---
+++ src/main/java/com/avbravo/jmoordbcorebookexample/repository/PersonaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.avbravo.jmoordbcorebookexample.repository;
+
+import com.avbravo.jmoordbcorebookexample.model.Persona;
+import com.jmoordb.core.annotation.repository.Repository;
+import com.jmoordb.core.repository.CrudRepository;
+
+/**
+ *
+ * @author avbravo
+ */
+@Repository(entity = Persona.class)
+public interface PersonaRepository extends CrudRepository<Persona, Long> {
+    
+}
