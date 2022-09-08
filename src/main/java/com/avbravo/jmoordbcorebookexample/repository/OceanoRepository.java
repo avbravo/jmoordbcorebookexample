77c45c270528e9767e2d9964c925309e281f2f4f
avbravo <avbravo@avbravo>
2022-09-08 16:02:57 -0500

    capitulo 4

---
+++ src/main/java/com/avbravo/jmoordbcorebookexample/repository/OceanoRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.avbravo.jmoordbcorebookexample.repository;
+
+import com.avbravo.jmoordbcorebookexample.model.Oceano;
+import com.jmoordb.core.annotation.repository.Repository;
+import com.jmoordb.core.repository.CrudRepository;
+
+/**
+ *
+ * @author avbravo
+ */
+@Repository(entity = Oceano.class)
+public interface OceanoRepository extends CrudRepository<Oceano, String> {
+    
+}
